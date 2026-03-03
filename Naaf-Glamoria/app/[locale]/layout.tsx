6c7ce791b0076610d5bef2c42c3d616d943fb169
Afeef-m <afeefmaliyekkal135@gmail.com>
2026-03-03 16:26:56 +0530

    Naaf-Glamoria

--- Naaf-Glamoria/app/[locale]/layout.tsx
+++ Naaf-Glamoria/app/[locale]/layout.tsx
@@ -33,7 +33,7 @@ export default async function LocaleLayout({
   params,
 }: {
   children: React.ReactNode;
-params: { locale: string };
+params:Promise< { locale: string }>;
 }) {
   const { locale } = await params;
   const finalLocale = locale === "ar" ? "ar" : "en";
